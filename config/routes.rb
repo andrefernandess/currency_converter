68fa8ed78276d093c2ab0d9f92e60258084b3872
Andre Fernandes <andre.l.fernandess@hotmail.com>
2026-01-29 11:17:35 -0300

    Change endpoint to create new conversion

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # Health Check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  #API routes
+  # API routes
   namespace :api do
     namespace :v1 do
-      resources :conversions, only: [:create]
+      post "convert", to: "conversions#create"
       resources :transactions, only: [:index]
     end
   end
